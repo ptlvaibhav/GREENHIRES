bb259f585feaaf2eb00503a7a7b2c2650ea0fcd8
Vaibhav Patel <vaibhavkpatel592@gmail.com>
2024-08-24 21:43:56 +0530

    Created Splash Screen,LoginActivity and implemented the login Code using the sharedprefrences concept

--- APPLICATION/app/src/main/java/com/agriconnect/agrilink/MainActivity.java
+++ APPLICATION/app/src/main/java/com/agriconnect/agrilink/MainActivity.java
@@ -1,12 +1,5 @@
 package com.agriconnect.agrilink;
 
-//import android.os.Bundle;
-//
-//import androidx.activity.EdgeToEdge;
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.core.graphics.Insets;
-//import androidx.core.view.ViewCompat;
-//import androidx.core.view.WindowInsetsCompat;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
