24d67942cb246f9e0d0f67236a14323f7e1ddc28
Vaibhav Patel <vaibhavkpatel592@gmail.com>
2024-09-21 04:06:40 +0530

    Created Adapter and Fragments for ChatActivity and some more little chnges like chngeing of fonts etc

---
+++ APPLICATION/app/src/main/java/com/agriconnect/agrilink/ChatViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.agriconnect.agrilink;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.agriconnect.agrilink.fragments.FarmerFragment;
+import com.agriconnect.agrilink.fragments.IndustriesFragment;
+import com.agriconnect.agrilink.fragments.LandlordFragment;
+import com.agriconnect.agrilink.fragments.MerchantFragment;
+
+public class ChatViewPagerAdapter extends FragmentStateAdapter {
+
+    public ChatViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    public ChatViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new FarmerFragment();
+            case 1:
+                return new MerchantFragment();
+            case 2:
+                return new LandlordFragment();
+            case 3:
+                return new IndustriesFragment();
+            default:
+                return new FarmerFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
--- APPLICATION/app/src/main/java/com/agriconnect/agrilink/PostjobActivity.java
+++ APPLICATION/app/src/main/java/com/agriconnect/agrilink/PostjobActivity.java
@@ -1,14 +1,16 @@
 package com.agriconnect.agrilink;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ChatsActivity extends AppCompatActivity {
+
+public class PostjobActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chats);
+        setContentView(R.layout.activity_postjob);
 
     }
 }
