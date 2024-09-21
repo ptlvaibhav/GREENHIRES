5a279edd87c62de8b455d9a962fa0e096c05c930
Vaibhav Patel <vaibhavkpatel592@gmail.com>
2024-09-22 02:21:18 +0530

    Solved the Errors!!

--- APPLICATION/app/src/main/java/com/agriconnect/agrilink/AirTableApiService.java
+++ APPLICATION/app/src/main/java/com/agriconnect/agrilink/AirTableApiService.java
@@ -8,10 +8,10 @@ import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface AirTableApiService {
-    @Headers("Authorization: Bearer pat0h7EQw0GwMJ7G8.a0e83d2a8caba436893708229e3183754b278b16e537c339b76df09fb37af951")
+    @Headers("Authorization: Bearer pat0h7EQw0GwMJ7G8.d3934955ea973a65713953b1d6688f844033bc8fd3bed03498656377ca0e0d2e")
     @GET("user")
     Call<AirtableResponse> getUser(@Query("filterByFormula") String filter);
-    @Headers({"Authorization: Bearer pat0h7EQw0GwMJ7G8.a0e83d2a8caba436893708229e3183754b278b16e537c339b76df09fb37af951",
+    @Headers({"Authorization: Bearer pat0h7EQw0GwMJ7G8.d3934955ea973a65713953b1d6688f844033bc8fd3bed03498656377ca0e0d2e",
             "Content-Type: application/json"})
     @POST("user")
     Call<AirtableResponse> createUser(@Body AirtableUser user);
